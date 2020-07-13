d459df39641f081037ff29d6d6db5e36e54e83d7
Misbah Khalil <misbah.khalil.az@gmail.com>
2020-07-13 11:58:50 +0500

    init

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
 import React from "react";
 import io from "socket.io-client";
 
-const socket = io.connect("http://localhost:4000");
+const socket = io.connect("localhost:4000");
 
 export default function App(props) {
-	// socket.emit("join", "class1");
 	socket.on("message", (msg) => console.log(msg));
 
 	return (
